a0fa10c3125eb07d7f02f7213e66d14f1909f0eb
Rodion Efremov <rodion.efremov@helsinki.fi>
2013-12-08 18:52:28 +0200

    Parallel BFS done.

--- src/main/java/net/coderodde/cskit/App.java
+++ src/main/java/net/coderodde/cskit/App.java
@@ -10,6 +10,7 @@ import static net.coderodde.cskit.Utilities.title;
 import net.coderodde.cskit.graph.DirectedGraphNode;
 import net.coderodde.cskit.p2psp.BidirectionalBreadthFirstSearchFinder;
 import net.coderodde.cskit.p2psp.BreadthFirstSearchFinder;
+import net.coderodde.cskit.p2psp.ParallelBidirectionalBFSFinder;
 import net.coderodde.cskit.p2psp.UniformCostPathFinder;
 /**
  * Hello from cskit.
@@ -23,10 +24,14 @@ public class App{
 
     public static void profileBreadthFirstSearchAlgorithms() {
         title("Uniform cost graph search");
-        final long SEED = 1386506122786L; //System.currentTimeMillis();
+        final long SEED = System.currentTimeMillis();
         final Random r = new Random(SEED);
-        final int SIZE = 10000;
+        final int SIZE = 50010;
         final float LOAD_FACTOR = 5.5f / SIZE;
+
+        System.out.println("Nodes in the graph: " + SIZE + ", load factor: "
+                + LOAD_FACTOR);
+
         System.out.println("Seed: " + SEED);
 
         List<DirectedGraphNode> graph =
@@ -41,6 +46,9 @@ public class App{
         UniformCostPathFinder finder2 =
                 new BidirectionalBreadthFirstSearchFinder();
 
+        UniformCostPathFinder finder3 =
+                new ParallelBidirectionalBFSFinder();
+
         long ta = System.currentTimeMillis();
         List<DirectedGraphNode> path1 = finder1.find(source, target);
         long tb = System.currentTimeMillis();
@@ -54,28 +62,46 @@ public class App{
         System.out.println("BidirectionalBreadthFirstSearchFinder in "
                 + (tb - ta) + " ms.");
 
+        ta = System.currentTimeMillis();
+        List<DirectedGraphNode> path3 = finder3.find(source, target);
+        tb = System.currentTimeMillis();
+
+        System.out.println("ParallelBidirectionalBFSFinder in "
+                + (tb - ta) + " ms.");
+
         line();
 
-        boolean eq = path1.size() == path2.size();
+        boolean eq = path1.size() == path2.size()
+                  && path2.size() == path3.size();
 
         if (eq == true) {
             System.out.println("Paths are of same length: " + eq
                     + ", length: " + path1.size());
         } else {
             System.out.println("Erroneous paths! Lengths: "
-                    + path1.size() + " and " + path2.size());
+                    + path1.size() + ", " + path2.size()
+                    + " and " + path3.size() + ".");
         }
 
         boolean ok1 = (isConnectedPath(path1)
                 && path1.get(0).equals(source)
                 && path1.get(path1.size() - 1).equals(target));
 
-        boolean ok2 = (isConnectedPath(path1)
+        boolean ok2 = (isConnectedPath(path2)
                 && path2.get(0).equals(source)
                 && path2.get(path2.size() - 1).equals(target));
 
-        System.out.println("Breadth-first search path o.k.: " + ok1);
-        System.out.println("Bidirectional breadth-first search path o.k.: " + ok2);
+        boolean ok3 = (isConnectedPath(path3)
+                && path3.get(0).equals(source)
+                && path3.get(path3.size() - 1).equals(target));
+
+
+
+        System.out.println("Breadth-first search path OK: " + ok1);
+        System.out.println("Bidirectional breadth-first search path OK: "
+                + ok2);
+        System.out.println("Bidirectional parallel BFS path OK: "
+                + ok3);
 
         line();
     }
